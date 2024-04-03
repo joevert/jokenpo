d2135f71b2c1e195a6cc7d77be4f86859da490d3
joevert <joao_marcos99@hotmail.com>
2024-04-03 20:15:23 -0300

    Add playgame function. not tested

--- script.js
+++ script.js
@@ -7,7 +7,6 @@
 function getcomputerchoice() {
     let computerchoice = '';
     number = Math.floor((Math.random() * 3) + 1);
-    console.log(number);
     if (number === 1) {
         computerchoice = 'ROCK';
     } else if (number === 2) {
@@ -37,32 +36,50 @@ function getuserchoice() {
 
 // compara os dois valores
 
-function roundjokenpo(computerchoice, userchoice) {
+function roundjokenpo(computerchoice, userchoice, score) {
     let result = '';
     if (computerchoice === userchoice) {
         return result = ('Foi um empate!');
     } else if ((computerchoice === 'ROCK' && userchoice === 'SCIZOR') 
     || (computerchoice === 'PAPER' && userchoice === 'ROCK') 
     || (computerchoice === 'SCIZOR' && userchoice === 'PAPER')) {
+        score--;
         return result = (`Voce perdeu! ${computerchoice} vence ${userchoice}`);            
-    } else return result = (`Voce ganhou ${userchoice} vence ${computerchoice}`);
+    } else {
+        score++;
+        return result = (`Voce ganhou ${userchoice} vence ${computerchoice}`);
+    }
             
         
 
 }
 
-// anota quem venceu
+//jogar o jogo 5x
 
-// mostra quem venceu
+function playgame() {
+    let score = 0;
+    for (let cont = 0; let < 5; let++) {
+        let computerSelection = getcomputerchoice();
+        let userSelection = getuserchoice();
+        let result = roundjokenpo(computerSelection, userSelection, score);
+    }
+    if (score > 0) {
+        console.log(`Parabens, voce venceu`);
+    } else {
+        console.log(`Infelizmente voce perdeu :()`);
+    }
+}
+
+
+//chama as funcoes
 
 let computerSelection = getcomputerchoice();
 
 let userSelection = getuserchoice();
 
-console.log(userSelection);
-
-console.log(computerSelection);
+// mostra quem venceu
 
 let result = roundjokenpo(computerSelection, userSelection);
 
-console.log (result);
+
+
